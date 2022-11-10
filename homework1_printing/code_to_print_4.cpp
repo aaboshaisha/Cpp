a28e0ce606ba6a693dffe98f1eaef6cd1e4b16c4
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2022-11-10 16:35:28 +0000

    Added homework 1 - printing - solutions.

---
+++ homework1_printing/code_to_print_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "    1    " << endl;
+    cout << "   232   " << endl;
+    cout << "  34543  " << endl;
+    cout << " 4567654 " << endl;
+    cout << "567898765" << endl;
+
+    return 0;
+}
